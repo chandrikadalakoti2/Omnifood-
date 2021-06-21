68e7dab121f2e60691c398e50db6b8b2ba794044
Chandrika Dalakoti <70463605+chandrikadalakoti2@users.noreply.github.com>
2021-06-21 18:31:35 +0530

    Add files via upload

---
+++ resources/javascript/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function()
+{
+    $('.js--section-features').waypoint(function(direction)
+    {
+        if(direction=='down')
+        {
+            $('nav').addClass('sticky');
+        }
+        else
+        {
+            $('nav').removeClass('sticky');
+        }
+    
+        offset:'20px;'
+    });
+
+    $('.js--scroll-to-plans').click(function(){
+        $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000);
+    });
+
+    $('.js--wp-1').waypoint(function(direction)
+    {
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{offset:'50%'}
+    );
+
+    $('.js--wp-2').waypoint(function(direction)
+    {
+        $('.js--wp-2').addClass('animated fadeIn');
+    },{offset:'50%'}
+    );
+
+    $('.js--wp-3').waypoint(function(direction)
+    {
+        $('.js--wp-3').addClass('animated fadeInUp');
+    },{offset:'50%'}
+    );
+
+    $('.js--wp-4').waypoint(function(direction)
+    {
+        $('.js--wp-4').addClass('animated pulse');
+    },{offset:'50%'}
+    );
+
+    $('.js--nav-icon').click(function(){
+        var nav=$('.js--main-nav');
+        nav.slideToggle(200);
+    });
+});
+
+    
+
