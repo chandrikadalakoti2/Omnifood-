a3b4d1069001339cce59e6415612b55e4cf39b5d
Chandrika Dalakoti <70463605+chandrikadalakoti2@users.noreply.github.com>
2021-06-22 15:58:08 +0530

    Add files via upload

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -14,13 +14,6 @@ $(document).ready(function()
         offset:'20px;'
     });
 
-    $('.js--scroll-to-plans').click(function(){
-        $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000);
-    });
-
-    $('.js--scroll-to-start').click(function(){
-        $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000);
-    });
 
     $('.js--wp-1').waypoint(function(direction)
     {
